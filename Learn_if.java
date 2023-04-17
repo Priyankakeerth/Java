ec75c4ff36f088d5f74e56be2f27b079aaf6853d
Priyanka keerth <priyankakeerthi.cs@gmail.com>
2023-04-17 11:57:48 +0530

    If condition

---
+++ Learn_if.java
@@ -0,0 +1,34 @@
+class Learn_if
+{
+public static void main(String[] args)
+{
+Learn_if li= new Learn_if();
+li.find_highest_score(90,95);
+
+}
+void find_highest_score(int vk, int msd)
+{
+
+//int vk=95;
+//int msd=100;
+if(vk>msd)
+{
+System.out.println("vk");
+}
+else if(msd>vk)
+{
+System.out.println("msd");
+}
+else
+{
+//System.out.println(vk + msd);
+System.out.println("vk");
+System.out.println("msd");
+}
+}
+
+
+
+}
+
+
